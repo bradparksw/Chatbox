354ccaafba124b646a1ea2a167b7257669e4322f
Sang Won Park <sw27park@edu.uwaterloo.ca>
2019-01-27 19:14:15 -0500

    Added username login functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+    usernames = [];
 
 server.listen(process.env.PORT || 3000);
 console.log('Server Running...');
@@ -13,9 +14,35 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
     console.log('Socket Connected...');
+
+    socket.on('new user', function(data, callback){
+        if(usernames.indexOf(data) != -1){
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+        }
+    });
+
+    // Update Usernames
+    function updateUsernames(){
+        io.sockets.emit('usernames', usernames);
+    }
     
     // Send Message
     socket.on('send message', function(data){
         io.sockets.emit('new message', {msg: data});
+    });
+
+    // Dsconnect
+    socket.on('disconnect', function(){
+      if(!socket.username){
+        return;
+      }
+
+      username.splice(usernames.indexOf(socket.username), 1);
+      updateUsernames();
     });
 });
